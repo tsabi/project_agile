41628922316872ac9e1886cd8884ca1f786daa4d
Petar Najman <petar.najman@modoolar.com>
2018-02-14 13:37:55 +0100

    Branch cleanning

--- project_key/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Modoolar <info@modoolar.com>
-# License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
-
-import models
-import controllers
-from hooks import post_init_hook
--- project_workflow/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Modoolar <info@modoolar.com>
-# License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
-
-import project_workflow
-import project
-import project_workflow_xml
-import project_workflow_importer
-import project_workflow_publisher
